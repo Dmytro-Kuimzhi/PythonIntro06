22677a8bdfae3b9277a0792b74e662e859c5eaf2
Dmytro-Kuimzhi <63404636+Dmytro-Kuimzhi@users.noreply.github.com>
2020-04-19 13:42:18 +0300

    Add files via upload

---
+++ Hometasks_2.py
@@ -0,0 +1,16 @@
+print(" Hometasks")
+    # Домашка готова :)
+    # болие отимального варианта пока не нашол
+
+print("""
+ Escape sequences
+ \\a \t Bell (alert)
+ \\b \t Backspace
+ \\n \t New line
+ \\t \t Horizontal tab
+ \\\\ \t Backslash \\
+ \\\" \t Double quotation mark \"
+ \\' \t Single quotation mark \'
+ """)
+
+input("") # просто люблю запускать двойным кликом
